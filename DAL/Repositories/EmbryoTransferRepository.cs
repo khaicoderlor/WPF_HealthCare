11ead79a5ca15853beb35934f1ec242d4af81428
Nguyenntse <NguyenNTSE180106@fpt.edu.vn>
2025-07-31 01:36:26 +0700

    backend ReportProgress

--- DAL/Repositories/EmbryoTransferRepository.cs
+++ DAL/Repositories/EmbryoTransferRepository.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL.Context;
 
 namespace DAL.Repositories
 {
-    internal class EmbryoTransferRepository
+    public class EmbryoTransferRepository
     {
+        private readonly AppDbContext _context;
+        public EmbryoTransferRepository()
+        {
+            _context = new AppDbContext();
+        }
+        public int GetEmbryoTransfersByOrderId(int orderId)
+        {
+            return _context.EmbryoTransfers
+                .Where(et => et.OrderId == orderId)
+                .Count();
+        } 
     }
 }
--- InfertilityCare/ReportProgressWindow.xaml.cs
+++ InfertilityCare/ReportProgressWindow.xaml.cs
@@ -11,15 +11,33 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using BLL.Services;
+using DAL.Entities;
 
 namespace InfertilityCare
 {
     
     public partial class ReportProgressWindow : Window
     {
+        public int orderId { get; set; }
+        private readonly EggService _eggService;
+        private readonly EmbryoService _embryoService;
+        private readonly EmbryoTransferService _embryoTransferService;
         public ReportProgressWindow()
         {
             InitializeComponent();
+            _eggService = new EggService();
+            _embryoService = new EmbryoService();
+            _embryoTransferService = new EmbryoTransferService();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            txtNumberEmbryo.Text = _embryoService.GetEmbryoGainedsByOrderId(orderId).ToString();
+            txtNumberFrozenEmbryo.Text = _embryoService.GetEmbryoGainedsByOrderId(orderId).ToString();
+            txtNumberOfEgg.Text = _eggService.GetEggGainedsByOrderId(orderId).ToString();
+            txtNumberTransferEmbryo.Text = _embryoTransferService.GetEmbryoTransfersByOrderId(orderId).ToString();
         }
 
     }
