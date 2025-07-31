9a25d09423c7ff6ca6f1779b680fb23676ca3de9
Nguyenntse <NguyenNTSE180106@fpt.edu.vn>
2025-07-31 09:56:12 +0700

    feat: AppointmentByStepOrder

---
+++ InfertilityCare/AppointmentByStepOrderWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BLL.Services;
+
+namespace InfertilityCare
+{
+    /// <summary>
+    /// Interaction logic for AppointmentByStepOrderWindow.xaml
+    /// </summary>
+    public partial class AppointmentByStepOrderWindow : Window
+    {
+        public int OrderStepId { get; set; }
+        private readonly AppointmentService _appointmentService;
+        public AppointmentByStepOrderWindow()
+        {
+            InitializeComponent();
+            LoadAppointmentByStepOrder();
+        }
+        public void LoadAppointmentByStepOrder()
+        {
+            dgAppointment.ItemsSource = _appointmentService.GetAppointmentsByOrderStepId(OrderStepId);
+        }
+    }
+}
